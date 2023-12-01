e7b08e05728329b2175640fdbf5b6b07a4b7d3de
Phowebing <phowebing@gmail.com>
2023-12-01 18:04:42 +0900

    feat : 기본 라우터 진행

--- src/pages/diary/DiaryAdd.js
+++ src/pages/diary/DiaryAdd.js
@@ -2,11 +2,18 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Main from "../../components/Main";
 import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
 
 const DiaryAdd = () => {
+  const navigate = useNavigate();
+  const handleClickMenu = () => {
+    navigate(-1);
+  };
   return (
     <>
-      <Header>Diary Add</Header>
+      <Header handleClickMenu={handleClickMenu} icon="bt_back.svg">
+        Diary Add
+      </Header>
       <Main>
         <h2 style={{ textAlign: "center", color: "red" }}>다이어리 추가화면</h2>
       </Main>
